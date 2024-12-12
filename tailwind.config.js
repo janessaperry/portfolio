940724e5c7664838976a688f843067469916a024
janessaperry <hello@janessaperry.com>
2024-12-12 14:19:17 -0800

    add transparent color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
+      transparent: "transparent",
       blue: {
         50: "#C4C1D5",
         300: "#292661",
