24de3e48805a116a95540aa1fc6d7ba5d8eb7ada
janessaperry <hello@janessaperry.com>
2024-12-12 14:21:41 -0800

    update project card component and add project page content

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,53 @@
+import { Link } from "react-router";
+import { ProjectDetails } from "../data/allProjects";
+
+interface ProjectCardProps {
+  projectDetails: ProjectDetails;
+  colorTheme?: string;
+  layout?: string;
+}
+
+function ProjectCard({
+  projectDetails,
+  colorTheme = "default",
+  layout = "vertical",
+}: ProjectCardProps) {
+  return (
+    <Link
+      to="/projects"
+      className={`card ${
+        colorTheme === "accent"
+          ? "p-6 bg-mauve-500 lg:basis-1/2"
+          : "py-6 lg:flex-row"
+      } ${layout} py-6 flex flex-col gap-8 rounded-3xl`}
+    >
+      <div className="md:basis-1/3">
+        <img
+          src={projectDetails.thumbnailInfo.filePath}
+          alt={projectDetails.thumbnailInfo.altText}
+          className="sm:w-1/2 md:w-full lg:max-h-56 xl:object-contain justify-self-center"
+        />
+      </div>
+
+      <div className="md:basis-2/3 flex flex-col gap-2">
+        <h2 className="text-pink-700">{projectDetails.title}</h2>
+        <p className="text-blue-500">{projectDetails.description}</p>
+
+        <ul className="flex flex-wrap gap-2">
+          {projectDetails.categories.map((category) => {
+            return (
+              <li
+                key={category}
+                className="text-blue-300 text-base leading-none	lowercase py-2 px-3 bg-mauve-300 border border-mauve-700 rounded-lg"
+              >
+                {category}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </Link>
+  );
+}
+
+export default ProjectCard;
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,9 +1,79 @@
+import { ProjectDetails, allProjects } from "../data/allProjects";
+import ShowcaseProject from "../components/ProjectCard";
+import ButtonLink from "../components/ButtonLink";
+import { useState } from "react";
+
 function Projects() {
+  const [filteredProjects, setFilteredProjects] = useState(allProjects);
+  const [activeFilter, setActiveFilter] = useState("All");
+
+  const handleFilterClick = (filter: string) => {
+    setActiveFilter(filter);
+    if (filter === "All") {
+      setFilteredProjects(allProjects);
+    } else {
+      const testingFilteredProjects: ProjectDetails[] = allProjects.filter(
+        (project) => project.categories.includes(filter)
+      );
+
+      setFilteredProjects(testingFilteredProjects);
+    }
+  };
+
   return (
-    <main>
-      <section>
-        <h1>Projects</h1>
-      </section>
+    <main className="w-full md:container py-4 px-4 sm:px-6">
+      <h1 className="sr-only">Projects</h1>
+
+      <div className="flex flex-col gap-4 md:flex-row md:gap-10">
+        <div className="flex flex-col gap-4 md:gap-10 md:basis-1/3">
+          <section className="p-6 flex flex-col gap-4 bg-blue-500 rounded-3xl">
+            <h2>&lt;explore&gt;</h2>
+            <p>
+              Discover my journey through code and creativity. Each project
+              reflects problem-solving, innovation, and a passion for crafting
+              seamless user experiences.
+            </p>
+          </section>
+        </div>
+
+        <div className="flex flex-col gap-4 md:gap-10 md:basis-2/3">
+          <section className="p-6 flex flex-col gap-4 bg-blue-500 rounded-3xl">
+            <h2>&lt;Projects&gt;</h2>
+            <p>Check out some of my recent work below. </p>
+
+            <div className="flex gap-8">
+              <ButtonLink
+                label="All"
+                handleClick={() => handleFilterClick("All")}
+                ariaPressed={activeFilter === "All"}
+              />
+              <ButtonLink
+                label="Development"
+                handleClick={() => handleFilterClick("Development")}
+                ariaPressed={activeFilter === "Development"}
+              />
+              <ButtonLink
+                label="Design"
+                handleClick={() => handleFilterClick("Design")}
+                ariaPressed={activeFilter === "Design"}
+              />
+            </div>
+
+            <div className="flex flex-col lg:flex-row gap-4">
+              {filteredProjects.map((project) => {
+                return (
+                  <ShowcaseProject
+                    key={project.id}
+                    projectDetails={project}
+                    colorTheme="accent"
+                    layout="vertical"
+                  />
+                );
+              })}
+            </div>
+          </section>
+        </div>
+      </div>
     </main>
   );
 }
