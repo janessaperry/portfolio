7572e77ca95dff52ca10b0629af512d68a8b6861
janessaperry <hello@janessaperry.com>
2024-12-12 14:20:08 -0800

    update projects data to include flag for showcase projects along with more data

---
+++ src/data/allProjects.tsx
@@ -0,0 +1,62 @@
+export interface ProjectDetails {
+  id: number;
+  showcase: boolean;
+  slug: string;
+  categories: string[];
+  thumbnailInfo: {
+    filePath: string;
+    altText: string;
+  };
+  title: string;
+  description: string;
+  skillsAndTools: string[];
+}
+
+export const allProjects: ProjectDetails[] = [
+  {
+    id: 1,
+    showcase: true,
+    slug: "press-start",
+    categories: ["Design", "Development"],
+    thumbnailInfo: {
+      filePath:
+        "/src/assets/images/mockups/press-start--collection--laptop.png",
+      altText: "Press Start laptop mockup preview",
+    },
+    title: "Press Start",
+    description:
+      "Designed and developed a full-stack web app that helps users keep track of their video game collection so they can decide what to play next. The site is fully responsive with a user-friendly interface and real-time data synchronization.",
+    skillsAndTools: [
+      "React",
+      "Sass",
+      "JavaScript",
+      "Node JS",
+      "Express",
+      "Knex",
+      "MySQL",
+      "GraphQL",
+      "Figma",
+    ],
+  },
+  {
+    id: 2,
+    showcase: true,
+    slug: "meta-hackathon",
+    categories: ["Development"],
+    thumbnailInfo: {
+      filePath: "/src/assets/images/mockups/iggy-discover--flow--mobile.png",
+      altText: "Meta Hackathon - Iggy Discover mobile mockup preview",
+    },
+    title: "Meta Hackathon",
+    description:
+      "Developed an Instagram Discovery Tool during a 24-hour hackathon with Meta, allowing users to search for accounts by uploading images and discovering similar content and profiles.",
+    skillsAndTools: [
+      "React",
+      "Sass",
+      "JavaScript",
+      "Node JS",
+      "Express",
+      "Figma",
+    ],
+  },
+];
