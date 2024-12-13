d3af5240c8c22c6fd10a89cb3f08e41ff1074a8c
janessaperry <hello@janessaperry.com>
2024-12-13 13:24:22 -0800

    create data file for skills and tools

---
+++ src/data/skillsAndTools.tsx
@@ -0,0 +1,45 @@
+interface SkillsAndTools {
+  skills: string[];
+  tools: string[];
+}
+
+export const skillsAndTools: SkillsAndTools = {
+  skills: [
+    "Front-end Development",
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "SASS",
+    "Tailwind CSS",
+    "Bootstrap",
+    "React",
+    "TypeScript",
+    "Back-end Development",
+    "Node JS",
+    "Express",
+    "MySQL",
+    "REST APIs",
+    "Git",
+    "Github",
+    "Agile",
+    "UI Design",
+    "Responsive Design",
+    "Visual Design",
+    "WordPress",
+    "CMS",
+  ],
+  tools: [
+    "Git",
+    "GitHub",
+    "VS Code",
+    "PHP Storm",
+    "Axios",
+    "Postman",
+    "Netlify",
+    "MySQL Workbench",
+    "Chrome DevTools",
+    "Figma",
+    "Affinity Designer",
+    "Afinity Photo",
+  ],
+};
