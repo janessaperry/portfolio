26a12741443ec97b66c8fc728e93c53a3c989f59
janessaperry <hello@janessaperry.com>
2024-12-17 17:00:06 -0800

    update vite config to use env variables

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env.VITE_BASE_URL': JSON.stringify(process.env.VITE_BASE_URL),
+  },
   plugins: [react()],
 })
