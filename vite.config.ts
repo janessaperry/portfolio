5f5837050bb72eebf2c64368d75839a05c93a6ca
janessaperry <hello@janessaperry.com>
2024-12-17 17:07:09 -0800

    undo updates to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    'process.env.VITE_BASE_URL': JSON.stringify(process.env.VITE_BASE_URL),
-  },
   plugins: [react()],
 })
